4707ba71395dff6208ee601d62e52ad45386ce6e
asemg <35200958+asemg@users.noreply.github.com>
2018-11-14 22:56:19 -0500

    Add files via upload

--- lipnet/lipreading/videos.py
+++ lipnet/lipreading/videos.py
@@ -4,8 +4,58 @@ from keras import backend as K
 from scipy import ndimage
 from scipy.misc import imresize
 import skvideo.io
-import dlib
-from lipnet.lipreading.aligns import Align
+#import dlib
+
+
+import numpy as np
+
+class Align(object):
+    def __init__(self, absolute_max_string_len=32, label_func=None):
+        self.label_func = label_func
+        self.absolute_max_string_len = absolute_max_string_len
+
+    def from_file(self, path):
+        with open(path, 'r') as f:
+            lines = f.readlines()
+        align = [(int(y[0])/1000, int(y[1])/1000, y[2]) for y in [x.strip().split(" ") for x in lines]]
+        self.build(align)
+        return self
+
+    def from_array(self, align):
+        self.build(align)
+        return self
+
+    def build(self, align):
+        self.align = self.strip(align, ['sp','sil'])
+        self.sentence = self.get_sentence(align)
+        self.label = self.get_label(self.sentence)
+        self.padded_label = self.get_padded_label(self.label)
+
+    def strip(self, align, items):
+        return [sub for sub in align if sub[2] not in items]
+
+    def get_sentence(self, align):
+        return " ".join([y[-1] for y in align if y[-1] not in ['sp', 'sil']])
+
+    def get_label(self, sentence):
+        return self.label_func(sentence)
+
+    def get_padded_label(self, label):
+        padding = np.ones((self.absolute_max_string_len-len(label))) * -1
+        return np.concatenate((np.array(label), padding), axis=0)
+
+    @property
+    def word_length(self):
+        return len(self.sentence.split(" "))
+
+    @property
+    def sentence_length(self):
+        return len(self.sentence)
+
+    @property
+    def label_length(self):
+        return len(self.label)
+
 
 class VideoAugmenter(object):
     @staticmethod
@@ -134,8 +184,8 @@ class Video(object):
             raise Exception('Video type not found')
 
     def process_frames_face(self, frames):
-        detector = dlib.get_frontal_face_detector()
-        predictor = dlib.shape_predictor(self.face_predictor_path)
+        #detector = dlib.get_frontal_face_detector()
+        #predictor = dlib.shape_predictor(self.face_predictor_path)
         mouth_frames = self.get_frames_mouth(detector, predictor, frames)
         self.face = np.array(frames)
         self.mouth = np.array(mouth_frames)
